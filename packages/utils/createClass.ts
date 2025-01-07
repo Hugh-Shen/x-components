063a868c41ef603d468287e775478171c3d0cee1
axiu <axiu@mengxiang.com>
2025-01-07 16:24:40 +0800

    feat: bem rules

--- packages/utils/createClass.ts
+++ packages/utils/createClass.ts
@@ -16,10 +16,22 @@ const _bem = (prefixName: string, blockSuffix: string, element: string, modifier
 }
 
 const createBEM = (prefixName: string) => {
-
+  const b = (blockSuffix: string) => _bem(prefixName, blockSuffix, '', '')
+  const e = (element: string) => _bem(prefixName, '', element, '')
+  const m = (modifier: string) => _bem(prefixName, '', '', modifier)
+  const be = (blockSuffix: string, element: string) => _bem(prefixName, blockSuffix, element, '')
+  const bm = (blockSuffix: string, modifier: string) => _bem(prefixName, blockSuffix, '', modifier)
+  const em = (element: string, modifier: string) => _bem(prefixName, '', element, modifier)
+  const bem = (blockSuffix: string, element: string, modifier: string) => _bem(prefixName, blockSuffix, element, modifier)
 
   return {
-
+    b,
+    e,
+    m,
+    be,
+    bm,
+    em,
+    bem
   }
 }
 
